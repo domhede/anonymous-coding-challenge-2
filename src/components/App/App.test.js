3000a2026eadd96f02d9b6e3d6fbd56f49952c46
Dom Hede <dom@bridgeapp.com.au>
2018-02-03 18:35:43 +1100

    Add dependencies for testing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount } from 'enzyme';
+
+import App from '../App';
+import Counter from './Counter';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
