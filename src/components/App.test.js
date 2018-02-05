aa656e81a47a1f3ad5635da92a72344e4bd2dab5
Dom Hede <dom@bridgeapp.com.au>
2018-02-06 10:16:35 +1100

    Change directory structure

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 
-import App from '../App';
+import App from './App';
 import Counter from './Counter';
 
 it('renders without crashing', () => {
